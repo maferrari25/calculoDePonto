09298cf096aa837f479a53f798355a3d903115b3
maferrari <adelinaferrarisouza2@hotmail.com>
2024-05-08 22:55:29 -0300

    Melhorias

--- app.js
+++ app.js
@@ -26,32 +26,52 @@ function calcularUltimoPonto() {
     var segundoPonto = document.getElementById("segundoPonto").value;
     var terceiroPonto = document.getElementById("terceiroPonto").value;
     var duracaoAlmoco = document.getElementById("duracaoAlmoco").value;
+    var totalExpediente = document.getElementById("totalExpediente").value;
 
     
     var minutosPrimeiroPonto = converterParaMinutos(primeiroPonto);
     var minutosSegundoPonto = converterParaMinutos(segundoPonto);
     var minutosTerceiroPonto = converterParaMinutos(terceiroPonto);
-   
+    var totalExpediente = converterParaMinutos(totalExpediente);
     
     
 
-   
     var tempoTrabalhadoAntesAlmoco = minutosSegundoPonto - minutosPrimeiroPonto;
 
-    var tempoRestanteTrabalho = (7 * 60) + 30 - tempoTrabalhadoAntesAlmoco; 
+    var tempoRestanteTrabalho = totalExpediente - tempoTrabalhadoAntesAlmoco; 
 
-    
     var minutosUltimoPonto = minutosTerceiroPonto + tempoRestanteTrabalho;
 
-   
+    var totalTrabalhado = tempoTrabalhadoAntesAlmoco + tempoRestanteTrabalho;
+
     var ultimoPontoHora = formatarHora(minutosUltimoPonto);
 
     var duracaoAlmoco = minutosTerceiroPonto - minutosSegundoPonto;
 
     var duracaoAlmoco = formatarHora(duracaoAlmoco);
 
+    var totalTrabalhado = formatarHora(totalTrabalhado);
 
-    document.getElementById("duracaoAlmoco").value = duracaoAlmoco;
     
+
+
+    
+    document.getElementById("duracaoAlmoco").value = duracaoAlmoco;
     document.getElementById("ultimoPonto").value = ultimoPontoHora;
 }
+
+
+
+function limpar() {
+    document.getElementById('primeiroPonto').value = '';
+    document.getElementById('segundoPonto').value = '';
+    document.getElementById('terceiroPonto').value = '';
+    document.getElementById('duracaoAlmoco').value = '';
+    document.getElementById('ultimoPonto').value = '';
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    document.getElementById('totalExpediente').value = '07:30';
+
+  });
